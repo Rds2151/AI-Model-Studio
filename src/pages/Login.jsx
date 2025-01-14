e4cbdee9d02fdec3e9f8d519948c54aeda96055b
nishant-64 <nishant.jadhav@64-squares.com>
2025-01-14 12:21:50 +0530

    .

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useAuth } from "../AuthContext";
 import { useNavigate } from "react-router-dom";
 
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
